498c832b7ecb893b54ec013bcb4803f7e71150fc
paco <paco@MacBook-Pro-de-pako.local>
2021-06-10 20:35:05 -0500

    iniciando express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.status(200).send('OK');
+});
+
+app.listen(PORT,()=>{
+    console.log('escuchando en puerto ',PORT);
+});
